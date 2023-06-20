cf6f8f54e6ea032b74b95c99cc4f373b2db5ab77
andrewgryan <andrew.ryan@metoffice.gov.uk>
2023-06-20 17:12:03 +0100

    add tip to remove wrapping

--- c-game/game.js
+++ c-game/game.js
@@ -71,6 +71,13 @@ varying vec2 v_texcoord;
 uniform sampler2D u_texture;
 
 void main() {
+    if (v_texcoord.x < 0.0 ||
+        v_texcoord.y < 0.0 ||
+        v_texcoord.x > 1.0 ||
+        v_texcoord.y > 1.0) {
+      discard;
+    }
+
     gl_FragColor = texture2D(u_texture, v_texcoord);     
 }`),
 };
